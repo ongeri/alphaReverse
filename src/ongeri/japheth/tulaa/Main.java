b12386bdb8e5911df8b6b2e367e26719cf287279
Japheth Ongeri <jg.ongeri@ubuni.solutions>
2018-06-21 12:29:01 +0300

    Initial commit

---
+++ src/ongeri/japheth/tulaa/Main.java
@@ -0,0 +1,23 @@
+package ongeri.japheth.tulaa;
+
+import java.util.Scanner;
+
+/**
+ * Aim is to reverse a string of case insensitive alphabetic characters embedded with other characters in such a way
+ * that non alphabetic chars are not affected
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        StringReverser stringReverser = new StringReverser();
+        String toBeReversed;
+        String reversedString;
+        Scanner sc = new Scanner(System.in);
+        //Prompt for input
+        System.out.println("Enter the string to be reversed");
+        // Get input
+        toBeReversed = sc.next();
+        //Pass string to reverser and get reversed string output
+        reversedString = stringReverser.reverseString(toBeReversed);
+    }
+}
---
+++ src/ongeri/japheth/tulaa/StringReverser.java
@@ -0,0 +1,14 @@
+package ongeri.japheth.tulaa;
+
+public class StringReverser {
+    /**
+     * A method that takes a string to be reversed, reverses the alphabetic characters and returns the reversed result string
+     *
+     * @param toBeReversed The string that needs to be reversed
+     * @return The reversed string
+     */
+    public String reverseString(String toBeReversed) {
+        String reversedString;
+        return reversedString;
+    }
+}
